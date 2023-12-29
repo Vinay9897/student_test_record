90507ac6734593a2ea5c895f04c3164d95ca2063
Vinayyad9897 <139324006+Vinayyad9897@users.noreply.github.com>
2023-12-29 16:16:16 +0530

    and zipkin and sleuth dependency

--- ConfigService/src/main/java/com/config/ConfigServiceApplication.java
+++ ConfigService/src/main/java/com/config/ConfigServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServiceApplication {
 
