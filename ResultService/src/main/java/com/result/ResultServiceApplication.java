76ef399fd286bfc34cd12c18f3804483fc9c17cc
Vinayyad9897 <139324006+Vinayyad9897@users.noreply.github.com>
2023-12-30 20:23:24 +0530

    write bootstrap.yml file for all services

--- ResultService/src/main/java/com/result/ResultServiceApplication.java
+++ ResultService/src/main/java/com/result/ResultServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
-public class ResultServiceApplication extends SpringBootServletInitializer {
+public class ResultServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResultServiceApplication.class, args);
